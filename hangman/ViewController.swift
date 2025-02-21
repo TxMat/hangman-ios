ca066e70fa4c8172518abb240e7fe8761ee2d71a
iii <iiii@yyyy.cm>
2025-02-21 14:09:14 +0100

    Initial Commit

---
+++ hangman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hangman
+//
+//  Created by Mathieu Ponal on 21/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
