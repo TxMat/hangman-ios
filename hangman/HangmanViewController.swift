fd2f2673d65b2d0c0318977eb61699c8f01767bc
Mathieu Ponal <mathieu.ponal@gmail.com>
2025-02-21 14:38:21 +0100

    preparing

---
+++ hangman/HangmanViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HangmanViewController.swift
+//  hangman
+//
+//  Created by Mathieu Ponal on 21/02/2025.
+//
+
+import UIKit
+
+class HangmanViewController: UIViewController {
+    
+    var Level: UInt8 = 0;
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Level)
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
--- hangman/ViewController.swift
+++ hangman/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let a = sender! as! HangmanViewController
+        a.Level = 2;
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
 
 }
 
